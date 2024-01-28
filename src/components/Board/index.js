0924809ecda5e578fd3c7f86366441e97a9ff895
Shreyas Shinde <shindeshreyasv@gmail.com>
2024-01-28 18:50:53 +0530

    Added Drawing feature

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,28 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Board = () => {
   const canvasRef = useRef(null);
+  const shouldDraw = useRef(false);
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
   useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const changeConfig = () => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    changeConfig();
+  }, [color, size]);
+
+  useLayoutEffect(() => {
     if (!canvasRef.current) return;
 
     const canvas = canvasRef.current;
@@ -12,8 +31,48 @@ const Board = () => {
     // when we are mounting i.e. Earlier when we created our canvas only small portion
     // was in use. But using this, whole screen is alloted for our canvas.
     canvas.width = window.innerWidth;
-    canvas.height = window.innerWeight;
+    canvas.height = window.innerHeight;
+
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
+
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      context.beginPath();
+      beginPath(e.clientX, e.clientY);
+    };
+
+    const handleMouseMove = (e) => {
+      if (!shouldDraw.current) {
+        return;
+      }
+      drawLine(e.clientX, e.clientY);
+    };
+
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
+
+  // console.log(color, size); testing if color is changing or not
+
   return <canvas ref={canvasRef}></canvas>;
 };
 
--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,15 +1,27 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./index.module.css";
 import { COLORS, MENU_ITEMS } from "@/constants";
+import { changeColor, changeBrushSize } from "@/slice/toolboxSlice";
+import cx from "classnames";
 
 const Toolbox = () => {
+  const dispatch = useDispatch();
+
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
 
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER;
 
-  const updateBrushSize = (e) => {};
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
+
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+  };
+
+  const updateColor = (newColor) => {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+  };
 
   return (
     <div className={styles.toolboxContainer}>
@@ -18,28 +30,46 @@ const Toolbox = () => {
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLACK,
+              })}
               style={{ backgroundColor: COLORS.BLACK }}
+              onClick={() => updateColor(COLORS.BLACK)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.RED,
+              })}
               style={{ backgroundColor: COLORS.RED }}
+              onClick={() => updateColor(COLORS.RED)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.GREEN,
+              })}
               style={{ backgroundColor: COLORS.GREEN }}
+              onClick={() => updateColor(COLORS.GREEN)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLUE,
+              })}
               style={{ backgroundColor: COLORS.BLUE }}
+              onClick={() => updateColor(COLORS.BLUE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.ORANGE,
+              })}
               style={{ backgroundColor: COLORS.ORANGE }}
+              onClick={() => updateColor(COLORS.ORANGE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.YELLOW,
+              })}
               style={{ backgroundColor: COLORS.YELLOW }}
+              onClick={() => updateColor(COLORS.YELLOW)}
             />
           </div>
         </div>
@@ -55,6 +85,7 @@ const Toolbox = () => {
               max={10}
               step={1}
               onChange={updateBrushSize}
+              value={size}
             />
           </div>
         </div>
