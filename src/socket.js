eaea6acb88c0e8beef6d6c4c3f295453e81c0549
Shreyas Shinde <shindeshreyasv@gmail.com>
2024-01-28 22:36:17 +0530

    Deployment Done

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://webdoodle-server.onrender.com"
+    : "http://localhost:5000";
 
-export const socket = io("http://localhost:5000");
+export const socket = io(URL);
