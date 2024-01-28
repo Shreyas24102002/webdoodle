db989258faf6721783cb2b65a0bb78487260fb30
Shreyas Shinde <shindeshreyasv@gmail.com>
2024-01-28 20:52:20 +0530

    Added multicolor functionality

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,26 +1,29 @@
 import { useSelector, useDispatch } from "react-redux";
+import cx from "classnames";
+
 import styles from "./index.module.css";
+
 import { COLORS, MENU_ITEMS } from "@/constants";
 import { changeColor, changeBrushSize } from "@/slice/toolboxSlice";
-import cx from "classnames";
+import { socket } from "@/socket";
 
 const Toolbox = () => {
   const dispatch = useDispatch();
-
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
-
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
-    activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER;
-
+    activeMenuItem === MENU_ITEMS.PENCIL ||
+    activeMenuItem === MENU_ITEMS.ERASER;
   const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
 
   const updateBrushSize = (e) => {
     dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+    socket.emit("changeConfig", { color, size: e.target.value });
   };
 
   const updateColor = (newColor) => {
     dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+    socket.emit("changeConfig", { color: newColor, size });
   };
 
   return (
@@ -74,7 +77,6 @@ const Toolbox = () => {
           </div>
         </div>
       )}
-
       {showBrushToolOption && (
         <div className={styles.toolItem}>
           <h4 className={styles.toolText}>Brush Size</h4>
